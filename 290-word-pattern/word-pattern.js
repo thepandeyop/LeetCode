04abdcb32fae46ec4546282dbfa877e378593cc5
Abhishek Pandey <116102773+thepandeyop@users.noreply.github.com>
2024-01-18 15:45:33 +0530

    Time: 60 ms (20.51%) | Memory: 41.8 MB (52.03%) - LeetSync

---
+++ 290-word-pattern/word-pattern.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+var wordPattern = function(pattern, s) {
+    let p = pattern.split('');
+    let str = s.split(' ');
+
+    let r1 = ''
+    for (let i = 0; i < p.length; i++) {
+        r1 += p.indexOf(p[i]);
+    }
+
+    let r2 = ''
+    for (let i = 0; i < str.length; i++) {
+        r2 += str.indexOf(str[i]);
+    }
+
+    return r1 === r2;
+};
