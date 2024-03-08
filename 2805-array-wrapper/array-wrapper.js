6cd35aa140632b0c6544fe8fbd9987ff56a2a5d7
Abhishek Pandey <116102773+thepandeyop@users.noreply.github.com>
2024-03-08 14:35:16 +0530

    Time: 55 ms (46.06%) | Memory: 50.2 MB (46.71%) - LeetSync

---
+++ 2805-array-wrapper/array-wrapper.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {void}
+ */
+var ArrayWrapper = function(nums) {
+    this.array = nums
+};
+
+ArrayWrapper.prototype.valueOf = function() {
+    return this.array.reduce((pre, cur)=>pre+cur,0)
+}
+
+ArrayWrapper.prototype.toString = function() {
+    return JSON.stringify(this.array)
+}
