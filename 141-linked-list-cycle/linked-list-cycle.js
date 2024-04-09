db9b62f59e6a3d9016a19a5288c0b2af565939bf
Abhishek Pandey <116102773+thepandeyop@users.noreply.github.com>
2024-04-09 21:45:19 +0530

    Time: 68 ms (36.82%) | Memory: 54.7 MB (10.76%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.js
+++ 141-linked-list-cycle/linked-list-cycle.js
@@ -11,20 +11,19 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    
- if(!head) return false;
-    
-    let slow = head;
-    let fast = head;
-    
-    while(fast && fast.next){
-        slow = slow.next;
-        fast = fast.next.next;
-        
-        if(slow === fast){
-            return true;
-        }
+  if (!head) return false
+
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next
+    fast = fast.next.next
+
+    if (slow === fast) {
+      return true
     }
-    return false;
-    
+  }
+
+  return false;
 };
