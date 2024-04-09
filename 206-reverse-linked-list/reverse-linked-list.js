abb485b0b246c04b1d416e9493088c0bbba7be85
Abhishek Pandey <116102773+thepandeyop@users.noreply.github.com>
2024-04-10 00:00:18 +0530

    Time: 60 ms (52.30%) | Memory: 52.1 MB (21.82%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.js
+++ 206-reverse-linked-list/reverse-linked-list.js
@@ -9,13 +9,18 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+
 var reverseList = function(head) {
-    if(head===null ||  head.next ===null) return head;
     
-    let last  = reverseList(head.next);
     
-    head.next.next = head;
-    head.next = null;
-    return last;
-    
+    let prev =null;
+    let cur = head;
+    while(cur!=null){
+        let temp = cur.next;
+        cur.next = prev;
+        prev = cur;
+        cur = temp;
+        
+    }
+    return prev;
 };
