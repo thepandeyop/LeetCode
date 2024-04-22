e50d192ff2ec180f3afca7963b9a9a4560bbdd83
Abhishek Pandey <116102773+thepandeyop@users.noreply.github.com>
2024-04-22 22:56:39 +0530

    Time: 66 ms (66.29%) | Memory: 53 MB (17.68%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.js
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.js
@@ -11,15 +11,20 @@
  * @return {void} Do not return anything, modify root in-place instead.
  */
 var flatten = function(root) {
-    if(!root)return [];
-    let s = [];
-    s.push(root);
-    while(s.length){
-        let node = s.pop();
-        if(node.right) s.push(node.right);
-        if(node.left) s.push(node.left);
-        if(s.length) node.right = s[s.length-1];
-        node.left = null;
+    if(root==null) return null;
+    if(root.left==null && root.right==null) return root;
+    let r = flatten(root.right);
+    if(!root.left){
+        root.right=r;
+    }
+    else{
+        let l=flatten(root.left);
+        root.right=l;
+        while(l.right!==null){
+            l=l.right;
+        }
+        l.right=r;
+        root.left=null;
     }
     return root;
 };
