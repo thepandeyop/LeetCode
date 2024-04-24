a855b4319ae283f4984e56310cd9db55d2a7095f
Abhishek Pandey <116102773+thepandeyop@users.noreply.github.com>
2024-04-24 22:41:17 +0530

    Time: 46 ms (86.97%) | Memory: 48.9 MB (49.08%) - LeetSync

---
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var check = function (nums) {
+  let result = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > nums[(i + 1) % nums.length]) {
+      result++;
+    }
+  }
+
+  return result <= 1;
+};
