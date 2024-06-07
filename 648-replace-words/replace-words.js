b164ad8dc592ea4a6e83329577ac2fd5b5a47c42
Abhishek Pandey <116102773+thepandeyop@users.noreply.github.com>
2024-06-07 12:18:16 +0530

    Time: 96 ms (83.06%) | Memory: 69.5 MB (48.76%) - LeetSync

--- 648-replace-words/replace-words.js
+++ 648-replace-words/replace-words.js
@@ -4,15 +4,53 @@
  * @return {string}
  */
 
- var replaceWords = function(dictionary, sentence) {
-     let arr  = sentence.split(" ");
-    for(let i=0; i< arr.length; i++){
-        for(let j=0; j< dictionary.length; j++){
-            if(dictionary[j] && arr[i].startsWith(dictionary[j])){
-                arr[i] = dictionary[j];
-            }
-        }
+class TrieNode{
+    constructor(word){
+        this.children = {};
+        this.isEnd = false;
     }
+}
+class Trie{
+    constructor(word){
+        this.root = new TrieNode();
+    }
+    
+    insert(word){
+     let temp = this.root;  
+        for(let i=0;i<word.length;i++){
+         if(! temp.children[word[i]]){
+             temp.children[word[i]] = new TrieNode(word[i]);
+         }   
+            temp = temp.children[word[i]];
+        }
+        temp.isEnd = true;
+        temp.isCompleteWord = word;
+    }
+    
+    search(word){
+     let temp = this.root; 
+          for(let i=0;i<word.length;i++){
+            if(! temp.children[word[i]]) return word;
+              
+              temp = temp.children[word[i]];
+              if(temp.isEnd){
+                  return temp.isCompleteWord;
+              }
+        }
+        return word;
+    }
+}
 
-    return arr.join(" ");
+
+
+ var replaceWords = function(dictionary, sentence) {
+     let arr = sentence.split(' ');
+     let trie = new Trie();
+     for(let word of dictionary){
+         trie.insert(word);
+     }
+     for(let i =0;i<arr.length;i++){
+         arr[i] = trie.search(arr[i]);
+     }
+     return arr.join(' ');
 };
