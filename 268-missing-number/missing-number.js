3aa071ac3d27b559bbe73c50ad29de5c0129a462
Abhishek Pandey <116102773+thepandeyop@users.noreply.github.com>
2024-07-06 13:16:37 +0530

    Time: 58 ms (66.60%) | Memory: 52 MB (20.76%) - LeetSync

--- 268-missing-number/missing-number.js
+++ 268-missing-number/missing-number.js
@@ -5,11 +5,19 @@
 // using bit
 var missingNumber = function(nums) {
    
-  let n = nums.length;
-    for(let i =0;i<nums.length;i++){
-        n ^= i^nums[i];
-         
+     const n = nums.length;
+    const arr = new Array(n + 1).fill(0);
+    
+    for (let i = 0; i < n; i++) {
+        arr[nums[i]] = 1;
     }
-   
-    return n;
+    
+    for (let i = 0; i <= n; i++) {
+        if (arr[i] === 0) {
+            return i;
+        }
+    }
+    
+    return -1;
+    
 };
